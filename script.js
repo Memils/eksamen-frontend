1cde5d8f1a981eb25fa41cf70c793466b54b9935
prøveeksamen-emil <emirim01@innlandetfylke.no>
2024-05-28 10:25:35 +0200

    ga samme overskrift på bok.html som jeg gjorde på index.html. Fikk også til å lage individe enkeltsider før bøkene, etter jeg gjorde noen endringer i hvordan scripten hentet inn bøkene.

--- script.js
+++ script.js
@@ -17,36 +17,34 @@ function loadBookList() {
                 bookItem.classList.add('book');
                 bookItem.textContent = `${book[1]} by ${book[2]}`;
                 bookItem.dataset.booknumber = book[4];
+                bookItem.addEventListener('click', () => {
+                    window.location.href = `bok.html?booknumber=${book[4]}`;
+                });
                 bookListElement.appendChild(bookItem);
             });
         });
-    }
+}
+
 function loadBookDetails() {
     const bookDetailsElement = document.getElementById('book-details');
     const urlParams = new URLSearchParams(window.location.search);
     const booknumber = urlParams.get('booknumber');
 
     if (booknumber) {
-
-        bookListElement.addEventListener('click', (event) => {
-            if (event.target.classList.contains('book')) {
-                const booknumber = event.target.dataset.booknumber;
-                fetch(`http://localhost:3000/Bok/${booknumber}`)
-                .then(response => response.json())
-                .then(data => {
-                    const book = data[0];
-                    const imagePath = `http://localhost:3000/${book[5]}`;
-                    bookDetailsElement.innerHTML = `
+        fetch(`http://localhost:3000/Bok/${booknumber}`)
+            .then(response => response.json())
+            .then(data => {
+                const book = data[0];
+                const imagePath = `http://localhost:3000/${book[5]}`;
+                bookDetailsElement.innerHTML = `
                     <h3>${book[1]}</h3>
                     <p>Author: ${book[2]}</p>
                     <p>ISBN: ${book[3]}</p>
                     <p>Book Number: ${book[4]}</p>
-                    <img src="${imagePath}" alt="Strekkkode for ${book[1]}">
-                    `;
-                });
-            }
-        });
+                    <img src="${imagePath}" alt="Strekkode for ${book[1]}">
+                `;
+            });
     } else {
-        bookDetailsElement.innerHTML = '<p>Beklager, vi fant ikke boken du leter etter.</p>'
+        bookDetailsElement.innerHTML = '<p>Beklager, vi fant ikke boken du leter etter.</p>';
     }
-};
+}
